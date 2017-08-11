901d249871be45d962f38466ec0bff10825b8b92
Jim Jeffers <jim@sumocreations.com>
2017-08-11 14:08:46 -0700

    Added conversion for pollutant and environment to specific target unit.

--- src/conversions.js
+++ src/conversions.js
@@ -1,4 +1,6 @@
 //@flow
+import { massForSubstance } from "./molecularWeight"
+import type { Unit, Pollutant, Environment } from "./types"
 
 export const ugm3ToPPB = (
   ugm3: number,
@@ -21,3 +23,36 @@ export const celciusToFarhenheit = (degreesCelcius: number): number =>
 
 export const farhenheitToCelcius = (degreesFarhenheit: number): number =>
   degreesFarhenheit / 1.8 - 32
+
+export const getTemperature = ({ temperature, unit }: Environment): number =>
+  unit === "C" ? temperature : farhenheitToCelcius(temperature)
+
+export const convertReadingToUnit = (
+  reading: Pollutant,
+  environment: Environment,
+  to: Unit
+): number => {
+  const { unit, amount } = reading
+  if (unit === to) {
+    return amount
+  }
+  const temperature = getTemperature(environment)
+  const molecularMass = massForSubstance(reading.substance)
+  const conversion = `${unit}|${to}`
+  switch (conversion) {
+    case "PPM|PPB":
+      return ppmToPPB(amount)
+    case "PPM|ug/m3":
+      return ppbToUGM3(ppmToPPB(amount), molecularMass, temperature)
+    case "PPB|PPM":
+      return ppbToPPM(amount)
+    case "PPB|ug/m3":
+      return ppbToUGM3(amount, molecularMass, temperature)
+    case "ug/m3|PPM":
+      return ppbToPPM(ugm3ToPPB(amount, molecularMass, temperature))
+    case "ug/m3|PPB":
+      return ugm3ToPPB(amount, molecularMass, temperature)
+    default:
+      throw new Error(`Could not convert '${conversion}'`)
+  }
+}
