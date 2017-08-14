553de3643d80e8e5b327e9834099acab2e80b596
Jim Jeffers <jim@sumocreations.com>
2017-08-14 13:18:22 -0700

    Updated types to support interval based ranges and only use supported substances from EPA documentation.

--- src/types.js
+++ src/types.js
@@ -1,27 +1,32 @@
 //@flow
 
-export type Substance =
-  | "BENZENE"
-  | "C6H5"
-  | "NO"
-  | "NO2"
-  | "O3"
-  | "PM2_5"
-  | "PM10"
+export type Substance = "CO" | "NO2" | "O3" | "SO2" | "PM2_5" | "PM10"
 
 export type Unit = "PPM" | "PPB" | "ug/m3"
 
+export type Interval = "1H" | "8H" | "24H"
+
+export type AirQualityDescription =
+  | "Good"
+  | "Moderate"
+  | "Unhealthy For Sensitive"
+  | "Unhealthy"
+  | "Very Unhealthy"
+  | "Hazardous"
+
 export type TemperatureScale = "F" | "C"
 
-export type Pollutant = {
-  substance: Substance,
-  amount: number,
-  unit: Unit
+export type Temperature = {
+  value: number,
+  unit: TemperatureScale
 }
 
-export type Environment = {
-  temperature: number,
-  unit: TemperatureScale
+export type AQSample = {
+  substance: Substance,
+  interval: Interval,
+  amount: number,
+  unit: Unit,
+  temperature?: Temperature
 }
 
 export type Range = {
@@ -29,22 +34,34 @@ export type Range = {
   low: number
 }
 
-export type PollutantRange = {
-  concentration: {
-    range: Range,
-    unit: Unit
-  },
-  aqiLevel: Range,
-  aqiDescription: string
+export type Concentration = {
+  range: Range,
+  unit: Unit,
+  interval: Interval
+}
+
+export type AQILevel = {
+  concentrations: Array<Concentration>,
+  aqi: Range,
+  aqiDescription: AirQualityDescription
+}
+
+export type AQISearchResult = {
+  aqiLevel: AQILevel,
+  concentration: Concentration
+}
+
+export type AQIResult = {
+  aqi: number,
+  description: string,
+  hexColor: string
 }
 
 export type RangeValues = {
-  O3: Array<PollutantRange>,
-  PM2_5: Array<PollutantRange>,
-  PM10: Array<PollutantRange>,
-  NO2: Array<PollutantRange>,
-  NO: Array<PollutantRange>,
-  SO2: Array<PollutantRange>,
-  C6H5: Array<PollutantRange>,
-  BENZENE: Array<PollutantRange>
+  O3: Array<AQILevel>,
+  PM2_5: Array<AQILevel>,
+  PM10: Array<AQILevel>,
+  NO2: Array<AQILevel>,
+  CO: Array<AQILevel>,
+  SO2: Array<AQILevel>
 }
