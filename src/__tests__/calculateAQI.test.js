17e92ad05c4bee05d85ec688f31ac617e45296ee
Jim Jeffers <jim@sumocreations.com>
2017-08-14 13:19:46 -0700

    Updated test suite per revised API.

--- src/__tests__/calculateAQI.test.js
+++ src/__tests__/calculateAQI.test.js
@@ -1,8 +1,15 @@
 import calculateAQI from "../calculateAQI"
 
 test("calculate moderate a ozone reading", () => {
-  const ozone = { substance: "O3", amount: 0.055, unit: "PPM" }
-  const environment = { temperature: 25, unit: "C" }
-  const aqi = calculateAQI(ozone, environment)
-  expect(aqi).toEqual(51)
+  const ozone = {
+    substance: "O3",
+    amount: 0.055,
+    unit: "PPM",
+    temperature: { value: 25, unit: "C" },
+    interval: "8H"
+  }
+  const result = calculateAQI(ozone)
+  expect(result.aqi).toEqual(51)
+  expect(result.description).toEqual("Moderate")
+  expect(result.hexColor).toEqual("#ff0")
 })
--- src/__tests__/conversions.test.js
+++ src/__tests__/conversions.test.js
@@ -12,41 +12,31 @@ test("it shuld accurately convert celcius to farhenheight", () => {
   expect(farhenheitToCelcius(77)).toEqual(25)
 })
 
-// CO 1 ppb = 1.145 μg/m3
-test("it should accurately convert CO from ppb to μg/m3", () => {
+// O3 1 ppb = 1.96 μg/m3
+test("it should accurately convert O3 from PPB to μg/m3 if using Farhenheight", () => {
   const result = convertReadingToUnit(
-    { amount: 1, unit: "PPB", substance: "CO" },
-    { temperature: 25, unit: "C" },
+    {
+      amount: 1,
+      unit: "PPB",
+      substance: "O3",
+      temperature: { value: 77, unit: "F" },
+      interval: "8H"
+    },
     "ug/m3"
   )
-  expect(Math.round(result * 100)).toEqual(115)
+  expect(Math.round(result * 100)).toEqual(196)
 })
 
-// Benzene 1 ppb = 3.19 μg/m3
-test("it should accurately convert Benzene from PPB to μg/m3", () => {
+// O3 1.96 μg/m3 = 1 ppb
+test("it should accurately convert O3 from μg/m3 to PPB", () => {
   const result = convertReadingToUnit(
-    { amount: 1, unit: "PPB", substance: "BENZENE" },
-    { temperature: 25, unit: "C" },
-    "ug/m3"
-  )
-  expect(Math.round(result * 100)).toEqual(319)
-})
-
-// Benzene 1 ppb = 3.19 μg/m3
-test("it should accurately convert Benzene from PPB to μg/m3 if using Farhenheight", () => {
-  const result = convertReadingToUnit(
-    { amount: 1, unit: "PPB", substance: "BENZENE" },
-    { temperature: 77, unit: "F" },
-    "ug/m3"
-  )
-  expect(Math.round(result * 100)).toEqual(319)
-})
-
-// Benzene 3.19 μg/m3 = 1 ppb
-test("it should accurately convert Benzene from μg/m3 to PPB", () => {
-  const result = convertReadingToUnit(
-    { amount: 3.19, unit: "ug/m3", substance: "BENZENE" },
-    { temperature: 25, unit: "C" },
+    {
+      amount: 1.96,
+      unit: "ug/m3",
+      substance: "O3",
+      temperature: { value: 25, unit: "C" },
+      interval: "8H"
+    },
     "PPB"
   )
   expect(Math.round(result * 100)).toEqual(100)
@@ -55,8 +45,13 @@ test("it should accurately convert Benzene from μg/m3 to PPB", () => {
 // SO2 1 ppb = 2.62 μg/m3
 test("it should accurately convert SO2 from ppb to μg/m3", () => {
   const result = convertReadingToUnit(
-    { amount: 1, unit: "PPB", substance: "SO2" },
-    { temperature: 25, unit: "C" },
+    {
+      amount: 1,
+      unit: "PPB",
+      substance: "SO2",
+      temperature: { value: 25, unit: "C" },
+      interval: "1H"
+    },
     "ug/m3"
   )
   expect(Math.round(result * 100)).toEqual(262)
@@ -65,8 +60,13 @@ test("it should accurately convert SO2 from ppb to μg/m3", () => {
 // SO2 2.62 μg/m3 = 1 ppb
 test("it should accurately convert SO2 from ppb to μg/m3", () => {
   const result = convertReadingToUnit(
-    { amount: 2.62, unit: "ug/m3", substance: "SO2" },
-    { temperature: 25, unit: "C" },
+    {
+      amount: 2.62,
+      unit: "ug/m3",
+      substance: "SO2",
+      temperature: { value: 25, unit: "C" },
+      interval: "1H"
+    },
     "PPB"
   )
   expect(Math.round(result * 100)).toEqual(100)
@@ -75,8 +75,13 @@ test("it should accurately convert SO2 from ppb to μg/m3", () => {
 // NO2 1 ppb = 1.88 μg/m3
 test("it should accurately convert NO2 from ppb to μg/m3", () => {
   const result = convertReadingToUnit(
-    { amount: 1, unit: "PPB", substance: "NO2" },
-    { temperature: 25, unit: "C" },
+    {
+      amount: 1,
+      unit: "PPB",
+      substance: "NO2",
+      temperature: { value: 25, unit: "C" },
+      interval: "1H"
+    },
     "ug/m3"
   )
   expect(Math.round(result * 100)).toEqual(188)
@@ -85,29 +90,29 @@ test("it should accurately convert NO2 from ppb to μg/m3", () => {
 // NO2 1.88 μg/m3 = 1 ppb
 test("it should accurately convert NO2 from μg/m3 to ppb", () => {
   const result = convertReadingToUnit(
-    { amount: 1.88, unit: "ug/m3", substance: "NO2" },
-    { temperature: 25, unit: "C" },
+    {
+      amount: 1.88,
+      unit: "ug/m3",
+      substance: "NO2",
+      temperature: { value: 25, unit: "C" },
+      interval: "8H"
+    },
     "PPB"
   )
   expect(Math.round(result * 100)).toEqual(100)
 })
 
-// NO 1 ppb = 1.25 μg/m3
+// CO 1 ppb = 1.14 μg/m3
 test("it should accurately convert NO from ppb to μg/m3", () => {
   const result = convertReadingToUnit(
-    { amount: 1, unit: "PPB", substance: "NO" },
-    { temperature: 25, unit: "C" },
+    {
+      amount: 1,
+      unit: "PPB",
+      substance: "CO",
+      temperature: { value: 25, unit: "C" },
+      interval: "24H"
+    },
     "ug/m3"
   )
-  expect(Math.round(result * 100)).toEqual(125)
-})
-
-// O3 1 ppb = 2.00 μg/m3
-test("it should accurately convert O3 from ppb to μg/m3", () => {
-  const result = convertReadingToUnit(
-    { amount: 1, unit: "PPB", substance: "O3" },
-    { temperature: 25, unit: "C" },
-    "ug/m3"
-  )
-  expect(Math.round(result * 100)).toEqual(200)
+  expect(Math.round(result * 100)).toEqual(114)
 })
